c6832f9a1742febacb01808b0e5554bd7c3a7cc6
wlsoo1234 <s2148250@siswa.um.edu.my>
2023-06-07 20:53:31 +0800

    Change MapSolver

--- Pixel Map/MapSolver.java
+++ Pixel Map/MapSolver.java
@@ -4,12 +4,19 @@ public class MapSolver {
 
     private static int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     private static int ROWS, COLS;
+    private static final int TARGET_STATIONS = 3;
+    private static int target_stations;
+    private static List<int[][]> images = new ArrayList<>();
 
+    private static int[][] getImages(int a){
+        return images.get(a);
+    }
+
+    private static int pathCount;
     public static void main(String[] args) {
         // Assuming you have loaded your 2D arrays into a list.
-        List<int[][]> images = new ArrayList<>();
 
-        int[][] map1 = { 
+        int[][] map1 = {
                 { 0, 0, 2, 0, 0, 0, 1, 1, 1, 1 },
                 { 0, 1, 0, 1, 1, 0, 0, 0, 1, 1 },
                 { 0, 1, 0, 1, 1, 0, 1, 0, 1, 1 },
@@ -95,52 +102,53 @@ public class MapSolver {
         images.add(map2);
         images.add(map3);
         images.add(map4);
+        int[] image_pathcounts = new int[5];
 
-        for (int[][] image : images) {
-            ROWS = image.length;
-            COLS = image[0].length;
-            int pathsCount = 0;
-            for (int i = 0; i < ROWS; i++) {
-                for (int j = 0; j < COLS; j++) {
-                    if (image[i][j] == 2) {
-                        pathsCount += dfs(image, i, j, 0, new boolean[ROWS][COLS]);
-                    }
-                }
-            }
-            System.out.println("Possible paths: " + pathsCount);
+        for (int i = 0; i < images.size(); i++) {
+            ROWS = images.get(i).length;
+            COLS = images.get(i)[0].length;
+            image_pathcounts[i] = findPaths(getImages(i), 3);
+            System.out.println("Possible paths: " + image_pathcounts[i]);
         }
+
+    }
+    
+
+    public static int findPaths(int[][] array, int targetStation) {
+        //System.out.println("Method findPaths(array, " + targetStation + ")");
+        pathCount = 0;
+        boolean[][] visited = new boolean[array.length][array[0].length];
+        target_stations = targetStation;
+        dfs(array, 0, 0, 0,visited);
+        return pathCount;
     }
 
-    private static int dfs(int[][] image, int row, int col, int stationsCount, boolean[][] visited) {
-        if (stationsCount > 3 || image[row][col] == 1 || visited[row][col]) {
-            return 0;
+
+    private static void dfs(int[][] image, int row, int col, int stationsCount, boolean[][] visited) {
+        int rows = image.length;
+        int columns = image[0].length;
+
+        if (row < 0 || row >= rows || col < 0 || col >= columns || visited[row][col] || image[row][col] == 1) {
+            return;
         }
 
-        if (image[row][col] == 3) {
-            if (stationsCount == 3) {
-                return 1;
-            } else {
-                return 0;
-            }
+        if (image[row][col] == 3 && stationsCount == target_stations) {
+            pathCount++;
+            return;
+        }
+
+        if (image[row][col] == 2) {
+            stationsCount++;
         }
 
         visited[row][col] = true;
-        int paths = 0;
 
         for (int[] direction : DIRECTIONS) {
-            int newRow = row + direction[0];
-            int newCol = col + direction[1];
-            if (isValid(newRow, newCol)) {
-                paths += dfs(image, newRow, newCol, image[row][col] == 2 ? stationsCount + 1 : stationsCount, visited);
-            }
+            rows = row + direction[0];
+            columns = col + direction[1];
+            dfs(image, rows, columns, stationsCount,visited);
         }
-
         visited[row][col] = false;
 
-        return paths;
-    }
-
-    private static boolean isValid(int row, int col) {
-        return row >= 0 && row < ROWS && col >= 0 && col < COLS;
     }
 }
